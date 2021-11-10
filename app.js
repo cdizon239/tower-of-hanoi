9779db8416eff8c664d51aa6849f69c5c67c1cb1
cdizon239 <coleencharmille@gmail.com>
2021-11-10 10:14:19 -0500

    initialize gamespace

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 //  Game defaults and stats
 let moves = 0;
-let totalNumDiscs = 3;
+let totalNumDiscs = 10;
+let towers = [[],[],[]]
 
-//  initialize 3 towers
-//  initialize disc
-const initGameSpace = () => {}
+let towerOne = document.querySelector('#towerOne')
+let towerTwo = document.querySelector('#towerTwo')
+let towerThree = document.querySelector('#towerThree')
+
+//  initialize discs
+const initGameSpace = () => {
+
+  document.querySelectorAll('.tower-top').forEach(tower => {
+    let discs = document.createElement('ul')
+    discs.classList.add('discs')
+    tower.append(discs)
+  })
+
+  for (let i=0; i<totalNumDiscs; i++) {
+    //  add to list
+    towers[0].push(i+1)
+    //  create the disc and append it to tower one -> tower-top (first-child of tower) -> ul(first-child of tower-top)
+    let disc = document.createElement('li')
+    disc.id = `disc${i+1}`
+    towerOne.firstElementChild.firstElementChild.append(disc)
+  }
+  console.log(towers);
+  
+}
+
+initGameSpace()
 
 //  handle clicks to discs
-
 const handleClickEvent = () => {}
 
 // check if move is valid
