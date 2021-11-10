cd5c50f845283713d9833e3d2e6505505f8012b7
cdizon239 <coleencharmille@gmail.com>
2021-11-10 00:32:50 -0500

    pseudocode

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
+//  Game defaults and stats
+let moves = 0;
+let totalNumDiscs = 3;
+
 //  initialize 3 towers
+//  initialize disc
+const initGameSpace = () => {}
 
-//  initialize disk
+//  handle clicks to discs
 
+const handleClickEvent = () => {}
+
+// check if move is valid
+// move is valid if tower has nothing in it yet, valid if disc 
+// erase from previous tower to move to a new one
+
+const checkMoveIfValid = () => {}
+
+const checkIfEmptyTower = () => {}
+
+const grabTopmostDisc = () => {}
+
+const checkWin = () => {}
+
+
+
+
+
+
+  
